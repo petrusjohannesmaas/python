24fbaa126f6154ccf2416dae8957151f4ec64cd7
PJ <petrusjohannesmaas@outlook.com>
2024-10-15 04:10:36 +0200

    Completed the daily_report.py. It's now updated to output JSON data reports that will be integrated with a database when I finish kpi.py

--- productivity/programs/daily_report.py
+++ productivity/programs/daily_report.py
@@ -16,30 +16,31 @@ def load_json_data(filename):
 
 """Variables and objects"""
 
-TACTICS = load_json_data("tactics.json")
+TIMEBLOCKS = load_json_data("timeblocks.json")
 
 # Assorted days of the week
-weekdays = jmespath.search("tactics.weekdays", TACTICS)
-fridays = jmespath.search("tactics.fridays", TACTICS)
-weekends = jmespath.search("tactics.weekends", TACTICS)
+weekdays = jmespath.search("timeblocks.weekdays", TIMEBLOCKS)
+fridays = jmespath.search("timeblocks.fridays", TIMEBLOCKS)
+weekends = jmespath.search("timeblocks.weekends", TIMEBLOCKS)
 
-# All tactics
-startup = jmespath.search("STARTUP.actions", weekdays)
-work = jmespath.search("WORK.actions", weekdays)
-shutdown = jmespath.search("SHUTDOWN.actions", weekdays)
-writing = jmespath.search("WRITING.actions", fridays)
-administration = jmespath.search("ADMINISTRATION.actions", weekends)
-family = jmespath.search("FAMILY.actions", weekends)
-personal = jmespath.search("PERSONAL.actions", weekends)
-marriage = jmespath.search("MARRIAGE.actions", weekends)
+# All timeblocks
+morning = jmespath.search("morning.actions", weekdays)
+work = jmespath.search("work.actions", weekdays)
+evening = jmespath.search("evening.actions", weekdays)
+research = jmespath.search("research.actions", fridays)
+sat_and_sun_morning = jmespath.search("morning.actions", weekends)
+family = jmespath.search("family.actions", weekends)
+personal = jmespath.search("personal.actions", weekends)
+marriage = jmespath.search("marriage.actions", weekends)
 
 """Functions for each slot"""
 
 
 def morning_slot():
     responses = []
-    for action in startup:
+    for action in morning:
         answer = console.input(f"{action}: [yes/no] ").strip().lower()
+        answer = "true" if answer == "yes" else "false"
         responses.append((action, answer))
     return responses
 
@@ -48,30 +49,34 @@ def work_slot():
     responses = []
     for action in work:
         answer = console.input(f"{action}: [yes/no] ").strip().lower()
+        answer = "true" if answer == "yes" else "false"
         responses.append((action, answer))
     return responses
 
 
-def shutdown_slot():
+def evening_slot():
     responses = []
-    for action in shutdown:
+    for action in evening:
         answer = console.input(f"{action}: [yes/no] ").strip().lower()
+        answer = "true" if answer == "yes" else "false"
         responses.append((action, answer))
     return responses
 
 
-def writing_slot():
+def research_slot():
     responses = []
-    for action in writing:
+    for action in research:
         answer = console.input(f"{action}: [yes/no] ").strip().lower()
+        answer = "true" if answer == "yes" else "false"
         responses.append((action, answer))
     return responses
 
 
-def admin_slot():
+def sat_and_sun_morning_slot():
     responses = []
-    for action in administration:
+    for action in sat_and_sun_morning:
         answer = console.input(f"{action}: [yes/no] ").strip().lower()
+        answer = "true" if answer == "yes" else "false"
         responses.append((action, answer))
     return responses
 
@@ -80,6 +85,7 @@ def family_slot():
     responses = []
     for action in family:
         answer = console.input(f"{action}: [yes/no] ").strip().lower()
+        answer = "true" if answer == "yes" else "false"
         responses.append((action, answer))
     return responses
 
@@ -88,6 +94,7 @@ def personal_slot():
     responses = []
     for action in personal:
         answer = console.input(f"{action}: [yes/no] ").strip().lower()
+        answer = "true" if answer == "yes" else "false"
         responses.append((action, answer))
     return responses
 
@@ -96,6 +103,7 @@ def marriage_slot():
     responses = []
     for action in marriage:
         answer = console.input(f"{action}: [yes/no] ").strip().lower()
+        answer = "true" if answer == "yes" else "false"
         responses.append((action, answer))
     return responses
 
@@ -118,9 +126,9 @@ if input_day in ["Monday", "Tuesday", "Wednesday", "Thursday"]:
     console.print("\nCheck your work.\nAnswer 'yes' if completed or 'no' if not.\n")
     responses.extend(work_slot())
     console.print(
-        "\nHow about the shutdown activities?.\nAnswer 'yes' if completed or 'no' if not.\n"
+        "\nHow about the evening activities?.\nAnswer 'yes' if completed or 'no' if not.\n"
     )
-    responses.extend(shutdown_slot())
+    responses.extend(evening_slot())
 
 elif input_day == "Friday":
     console.print(
@@ -128,19 +136,19 @@ elif input_day == "Friday":
     )
     responses.extend(morning_slot())
     console.print(
-        "\nDid you do your writing for the week?\nAnswer 'yes' if completed or 'no' if not.\n"
+        "\nDid you do your research for the week?\nAnswer 'yes' if completed or 'no' if not.\n"
     )
-    responses.extend(writing_slot())
+    responses.extend(research_slot())
     console.print(
-        "\nHow about the shutdown activities?.\nAnswer 'yes' if completed or 'no' if not.\n"
+        "\nHow about the evening activities?.\nAnswer 'yes' if completed or 'no' if not.\n"
     )
-    responses.extend(shutdown_slot())
+    responses.extend(evening_slot())
 
 elif input_day in ["Saturday", "Sunday"]:
     console.print(
         "\nLet's start with your morning.\nAnswer 'yes' if completed or 'no' if not.\n"
     )
-    responses.extend(admin_slot())
+    responses.extend(sat_and_sun_morning_slot())
     console.print(
         "\nDid you serve your family?.\nAnswer 'yes' if completed or 'no' if not.\n"
     )
@@ -157,10 +165,14 @@ elif input_day in ["Saturday", "Sunday"]:
 else:
     pass
 
-console.print("\n[bold]Were there any exceptions?[/bold] \n")
-exceptions = console.input("Explain the exception briefly: ")
-mood = console.input("\nWas it a good day?: (yes/no) ")
-comments = console.input("\nAny comments?: \n ->")
+mood = console.input("\n[bold]Was it a good day?:[/bold] (yes/no) \n-> ")
+mood = "true" if mood.lower() == "yes" else "false"
+
+exceptions = console.input(
+    "\n[bold]Were there any exceptions?:[/bold] (Brief explanation)\n-> "
+)
+comments = console.input("\n[bold]Any comments?:[/bold]\n-> ")
+memory = console.input("\n[bold]Note a memory you'd like to retain:[/bold]\n-> ")
 
 # Get current datetime
 now = datetime.now()
@@ -170,17 +182,29 @@ if not os.path.exists("../reports"):
     os.makedirs("../reports")
 
 # Create the file path
-file_name = f"tactical_report_{input_day}_{now.strftime('%B_%d_%Y')}.txt"
+file_name = f"daily_report_{input_day}_{now.strftime('%B_%d_%Y')}.json"
 file_path = os.path.join("../reports", file_name)
+
+# Calculate performance score
+total_actions = len(responses)
+true_actions = sum(1 for _, result in responses if result == "true")
+performance_score = (true_actions / total_actions) * 100
+
+# Create the report data
+report_data = {
+    "date": now.strftime("%Y-%m-%d"),
+    "day": input_day,
+    "completed": {action: result for action, result in responses},
+    "performance_score": performance_score,
+    "good_day": mood,
+    "exceptions": exceptions,
+    "comments": comments,
+    "memory": memory,
+}
 
 # Write to the file
 with open(file_path, "w") as file:
-    file.write(f"Tactical Report for {input_day}, {now.strftime('%B, %d, %Y')}\n\n")
-    for action, result in responses:
-        file.write(f"{action}: {result}\n")
-    file.write(f"\nExceptions: {exceptions}\n")
-    file.write(f"Mood: {mood}\n")
-    file.write(f"Comments: {comments}\n")
+    json.dump(report_data, file, indent=4)
 
 # Print the path to the file
 print(f"\nReport saved to {file_path}")
